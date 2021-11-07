f04fee419c3da704363a2d3875fe45b9ffb5903a
Your Name <you@example.com>
2021-11-07 04:14:19 -0500

    commit

--- emtester.py
+++ emtester.py
@@ -6,16 +6,16 @@ from sendgrid.helpers.mail import (
 from sendgrid import SendGridAPIClient
 
 to_emails=[
-    ('customer@example1.com', 'customer1'),
+    ('customer@example1.com', 'customer1'),     #customer email IDs and names must be inserted here
     ('customer@example2.com', 'customer2'),
     ('customer@example3.com', 'customer3')
 ]
 
 
 message = Mail(
-    from_email=('company1@example.com', 'company1'),
+    from_email=('company1@example.com', 'company1'),   #user email ID must be inserted here
     to_emails= to_emails,
-    subject='How was your recent order with us ?',
+    subject='How was your recent order with us ?',     
     html_content='<strong>Thank you for shopping with us and  we hope you enjoyed your purchase. Our ultimate goal is to provide our customers with the best serivce possible and that includes finding out areas we can improve.<br> We deeply appreciate your support and would value your input.<br></strong><em>Please reply to this email with your feedback<em>')
 
 try:

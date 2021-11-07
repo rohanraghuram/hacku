fccb2b9f62a2105f9753d55d2d6bee8cabdd987a
Your Name <you@example.com>
2021-11-07 02:56:43 -0500

    commit

--- emtester.py
+++ emtester.py
@@ -6,31 +6,18 @@ from sendgrid.helpers.mail import (
 from sendgrid import SendGridAPIClient
 
 to_emails=[
-    #('rohanraghuram37@gmail.com', 'Rohan Raghuram'),
-    #('raghuramkanakatte@gmail.com', 'KG Raghuram'),
-    ('vedant.bajaj14@gmail.com', 'Vedant Bajaj')
+    ('customer@example1.com', 'customer1'),
+    ('customer@example2.com', 'customer2'),
+    ('customer@example3.com', 'customer3')
 ]
 
 
 message = Mail(
-    from_email=('rraghuram@umass.edu', 'company1'),
+    from_email=('company1@example.com', 'company1'),
     to_emails= to_emails,
     subject='How was your recent order with us ?',
     html_content='<strong>Thank you for shopping with us and  we hope you enjoyed your purchase. Our ultimate goal is to provide our customers with the best serivce possible and that includes finding out areas we can improve.<br> We deeply appreciate your support and would value your input.<br></strong><em>Please reply to this email with your feedback<em>')
-'''
-file_path = 'unnamed.png'
-with open(file_path, 'rb') as f:
-    data = f.read()
-    f.close()
-encoded = base64.b64encode(data).decode()
-attachment = Attachment()
-attachment.file_content = FileContent(encoded)
-attachment.file_type = FileType('application/png')
-attachment.file_name = FileName('test_filename.png')
-attachment.disposition = Disposition('attachment')
-attachment.content_id = ContentId('Example Content ID') 
-#message.attachment = attachment
-'''
+
 try:
     sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sendgrid_client.send(message)
